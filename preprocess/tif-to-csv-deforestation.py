24cf357f361abe18cd7cf8d58461e7f9410e0801
Simon Barras <simon.barras@epfl.ch>
2026-03-17 19:18:27 +0100

    feat(tif to csv): Add country code to tile in deforestation

--- preprocess/tif-to-csv-deforestation.py
+++ preprocess/tif-to-csv-deforestation.py
@@ -1,5 +1,5 @@
 import argparse
-
+import reverse_geocoder as rg
 import rasterio
 import numpy as np
 import pandas as pd
@@ -12,6 +12,7 @@ cause = {
     4.0: "Wildfire",
     5.0: "Urbanization",
 }
+
 
 def main(tif_input, output_csv):
     with rasterio.open(tif_input) as src:
@@ -31,11 +32,20 @@ def main(tif_input, output_csv):
     rows, cols = np.indices(data.shape)
     xs, ys = rasterio.transform.xy(transform, rows.ravel(), cols.ravel())
 
-    df = pd.DataFrame({
-        "lon": xs,
-        "lat": ys,
-        "value": data.ravel()
-    })
+    # Add the county of each pixel based on its coordinates
+    countries = []
+    coords = list(zip(ys, xs))  # reverse to (lat, lon) for reverse_geocoder
+    countries = rg.search(coords)
+    print(f"Unique countries in data: {len(set(c['cc'] for c in countries))}")
+
+    df = pd.DataFrame(
+        {
+            "lon": xs,
+            "lat": ys,
+            "value": data.ravel(),
+            "country": [c["cc"] for c in countries],
+        }
+    )
 
     # Drop rows where value is NaN (was < 0)
     df = df.dropna(subset=["value"])
@@ -47,7 +57,9 @@ def main(tif_input, output_csv):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert a GeoTIFF file to a CSV with x, y, and value columns.")
+    parser = argparse.ArgumentParser(
+        description="Convert a GeoTIFF file to a CSV with x, y, and value columns."
+    )
     parser.add_argument("--input_tif", "-i", help="Path to the input GeoTIFF file")
     parser.add_argument("--output_csv", "-o", help="Path to the output CSV file")
     args = parser.parse_args()
