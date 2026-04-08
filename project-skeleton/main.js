5c32b2265b6f719936aa972c3771f70003db21e8
Olivier Dadoud <oliver.dad@tinquiete.com>
2026-04-08 11:27:52 +0200

    milestone 2 started skeleton

---
+++ project-skeleton/main.js
@@ -0,0 +1,22 @@
+
+/*
+	Run the action when we are sure the DOM has been loaded
+	https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+	Example:
+	whenDocumentLoaded(() => {
+		console.log('loaded!');
+		document.getElementById('some-element');
+	});
+*/
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
+
+whenDocumentLoaded(() => {
+	//do stuff
+});
